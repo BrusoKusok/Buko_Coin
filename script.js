6d94424160844b436753351a05cb38b9d6b6da67
BrusoKusok <108999056+BrusoKusok@users.noreply.github.com>
2024-05-31 03:35:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let clickCount = 0;
+let profitPerClick = 2;
+let nextLevel = 25000;
+let profitPerHour = 0;
+let level = 1;
 
-document.getElementById('clickButton').addEventListener('click', () => {
-    clickCount++;
-    document.getElementById('clickCount').innerText = clickCount;
+document.getElementById('click-circle').addEventListener('click', () => {
+    clickCount += profitPerClick;
+    document.getElementById('profit-per-click').innerText = `+${profitPerClick}`;
+    document.getElementById('next-level').innerText = `${nextLevel - clickCount}к`;
+    document.getElementById('profit-per-hour').innerText = profitPerHour;
+    document.getElementById('level').innerText = level;
+    if (clickCount >= nextLevel) {
+        level++;
+        clickCount = 0;
+        profitPerClick += 2;
+        nextLevel *= 2;
+        document.getElementById('level').innerText = level;
+        document.getElementById('next-level').innerText = `${nextLevel - clickCount}к`;
+    }
 });
